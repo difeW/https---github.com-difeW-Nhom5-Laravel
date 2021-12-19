25c9a5edec6cd90466b42819f22aab4ef92b6c62
muse <nangtho.muse@gmail.com>
2021-12-19 13:21:15 +0700

    Huy update

--- resources/views/pages/sanpham/show_details.blade.php
+++ resources/views/pages/sanpham/show_details.blade.php
@@ -6,30 +6,6 @@
 							<div class="view-product">
 								<img src="{{ URL::to('public/uploads/product/'.$value->product_image)}}" alt="" />
 							</div>
-							<div id="similar-product" class="carousel slide" data-ride="carousel">
-								
-								  <!-- Wrapper for slides -->
-								    <div class="carousel-inner">
-
-										<div class="item active">
-										  <a href=""><img src="{{URL::to('frontend/images/similar1.jpg')}}" alt=""></a>
-										  <a href=""><img src="{{URL::to('frontend/images/similar2.jpg')}}" alt=""></a>
-										  <a href=""><img src="{{URL::to('frontend/images/similar3.jpg')}}" alt=""></a>
-										</div>
-										
-										
-										
-									</div>
-
-								  <!-- Controls -->
-								  <a class="left item-control" href="#similar-product" data-slide="prev">
-									<i class="fa fa-angle-left"></i>
-								  </a>
-								  <a class="right item-control" href="#similar-product" data-slide="next">
-									<i class="fa fa-angle-right"></i>
-								  </a>
-							</div>
-
 						</div>
 						<div class="col-sm-7">
 							<div class="product-information"><!--/product-information-->
@@ -42,20 +18,20 @@
 									@csrf
 									<input type="hidden" value="{{$value->product_id}}" class="cart_product_id_{{$value->product_id}}">
 
-                                            <input type="hidden" value="{{$value->product_name}}" class="cart_product_name_{{$value->product_id}}">
+                                    <input type="hidden" value="{{$value->product_name}}" class="cart_product_name_{{$value->product_id}}">
 
-                                            <input type="hidden" value="{{$value->product_image}}" class="cart_product_image_{{$value->product_id}}">
+                                    <input type="hidden" value="{{$value->product_image}}" class="cart_product_image_{{$value->product_id}}">
 
-                                            <input type="hidden" value="{{$value->product_price}}" class="cart_product_price_{{$value->product_id}}">
+                                    <input type="hidden" value="{{$value->product_price}}" class="cart_product_price_{{$value->product_id}}">
                                           
-								<span>
-									<span>{{number_format($value->product_price,0,',','.').'VNĐ'}}</span>
+									<span>
+										<span>{{number_format($value->product_price,0,',','.').'VNĐ'}}</span>
 								
-									<label>Số lượng:</label>
-									<input name="qty" type="number" min="1" class="cart_product_qty_{{$value->product_id}}"  value="1" />
-									<input name="productid_hidden" type="hidden"  value="{{$value->product_id}}" />
-								</span>
-								<input type="button" value="Thêm giỏ hàng" class="btn btn-primary btn-sm add-to-cart" data-id_product="{{$value->product_id}}" name="add-to-cart">
+										<label>Số lượng:</label>
+										<input name="qty" type="number" min="1" class="cart_product_qty_{{$value->product_id}}"  value="1" />
+										<input name="productid_hidden" type="hidden"  value="{{$value->product_id}}" />
+									</span>
+									<input type="button" value="Thêm giỏ hàng" class="btn add-to-cart" data-id_product="{{$value->product_id}}" name="add-to-cart">
 								</form>
 
 								<p><b>Tình trạng:</b> Còn hàng</p>
@@ -92,20 +68,23 @@
 								<div class="col-sm-12">
 									<ul>
 										<li><a href=""><i class="fa fa-user"></i>EUGEN</a></li>
-										<li><a href=""><i class="fa fa-clock-o"></i>12:41 PM</a></li>
-										<li><a href=""><i class="fa fa-calendar-o"></i>31 DEC 2014</a></li>
+										<li><a href=""><i class="fa fa-clock-o"></i>1:23 PM</a></li>
+										<li><a href=""><i class="fa fa-calendar-o"></i>24 12 2021</a></li>
 									</ul>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-									<p><b>Write Your Review</b></p>
+									<p>
+										Chân thành cảm ơn quý khách đã mua sản phẩm từ MuseTech, mọi đánh giá của quý khách sẽ giúp cho thương hiệu 
+										MuseTech có thể hoàn thiện và phục vụ quý khách ngày một tốt hơn trong mai sau. Trân trọng!
+									</p>
+									<p><b>Viết đánh giá sản phẩm</b></p>
 									
 									<form action="#">
 										<span>
-											<input type="text" placeholder="Your Name"/>
-											<input type="email" placeholder="Email Address"/>
+											<input type="text" placeholder="Tên của bạn"/>
+											<input type="email" placeholder="Địa chỉ email"/>
 										</span>
 										<textarea name="" ></textarea>
-										<b>Rating: </b> <img src="images/product-details/rating.png" alt="" />
-										<button type="button" class="btn btn-default pull-right">
+										<!-- <b>Rating: </b> <img src="images/product-details/rating.png" alt="" /> -->
+										<button type="button" class="btn" style="border-radius: 10px;">
 											Submit
 										</button>
 									</form>
@@ -115,7 +94,7 @@
 						</div>
 					</div><!--/category-tab-->
 	@endforeach
-					<div class="recommended_items"><!--recommended_items-->
+					<!-- <div class="recommended_items">
 						<h2 class="title text-center">Sản phẩm liên quan</h2>
 						
 						<div id="recommended-item-carousel" class="carousel slide" data-ride="carousel">
@@ -143,9 +122,9 @@
 							</div>
 									
 						</div>
-					</div><!--/recommended_items-->
+					</div>
 					{{--   <ul class="pagination pagination-sm m-t-none m-b-none">
                        {!!$relate->links()!!}
-                      </ul> --}}
+                      </ul> --}} -->
 
-@endsection
+	@endsection
